0dbd509c24dd18ec15db1f16593525c2c6f6ee16
ThalitaRospendowski <thalita_rospendowiski@hotmail.com>
2023-03-08 16:51:18 -0300

    Repositório Aulas Front End II

---
+++ Aula01/JS/script.js
@@ -0,0 +1,8 @@
+var nome  = prompt("Qual é o seu nome?");
+var idade = prompt("Qual é a sua idade?");
+var job = prompt("Qual é o seu job?");
+
+const message = `Seja bem vindo ${nome}, você possui ${idade} anos.  Parabéns por trabalhar com ${job}`
+
+alert(message)
+
---
+++ Aula04/scripts.js
@@ -0,0 +1,18 @@
+//Pai
+
+console.log(document.querySelector("body"));
+
+     //Filho
+
+      console.log(document.querySelector("h1"));
+
+      console.log(document.getElementsByClassName("conteudo"));
+
+
+         // Filho do Filho Div
+
+        console.log(document.querySelector("h5"));
+        console.log(document.querySelector("ul"));
+
+
+
