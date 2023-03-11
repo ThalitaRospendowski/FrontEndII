80a6fe6c7f44d428b2be3eb1f00dca40c40b5860
ThalitaRospendowski <thalita_rospendowiski@hotmail.com>
2023-03-11 11:25:54 -0300

    FrontEndII - CheckPointI - 11/03/2023

---
+++ CheckPointI/script.js
@@ -0,0 +1,77 @@
+let button = document.querySelector('button');
+let element = document.querySelector('body');
+let elementcard = document.querySelector('#cards');
+const form = document.querySelector('form');
+const textTitulo = document.querySelector('titulo')
+const textUrl = document.querySelector('url')
+const textDesc = document.querySelector('desc')
+
+
+button.addEventListener('click',testebotao);
+
+function testebotao(){
+    console.log("Teste botão");
+}
+
+
+
+function checkTitulo{}
+
+
+const personagens = [
+    {
+        Titulo:'aaaaaaaaaaaaaaaaaaaaaa',
+        URL:'/imgs/img01.jpg',
+        Descricao:'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+
+    },
+    {
+        Titulo:'aaaaaaaaaaaaaaaaaaaaaa',
+        URL:'/imgs/img01.jpg',
+        Descricao:'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+
+    },
+    {
+        Titulo:'aaaaaaaaaaaaaaaaaaaaaa',
+        URL:'/imgs/img01.jpg',
+        Descricao:'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+
+    },       
+    {
+        Titulo:'aaaaaaaaaaaaaaaaaaaaaa',
+        URL:'/imgs/img01.jpg',
+        Descricao:'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbdddddddddfsdgdgggdgdgdgdgdfg'
+
+    },    
+    {
+        Titulo:'aaaaaaaaaaaaaaaaaaaaaa',
+        URL:'/imgs/img01.jpg',
+        Descricao:'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+
+    },    
+    {
+        Titulo:'aaaaaaaaaaaaaaaaaaaaaa',
+        URL:'/imgs/img01.jpg',
+        Descricao:'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbDSFSDFSDFSFSFSFFFFFFFSFDFSFSFSFSFSFSFSFSFDSFSFSF'
+
+    },                 
+]
+
+console.log(elementcard);
+
+
+for(let personagem of personagens){
+    elementcard.innerHTML += `
+    <div class="card">
+       <div class="card-header">
+           <img src="${personagem.URL}" class="card-img"/>
+       </div>
+       <div class="card-body">
+           <h2 class="card-titulo">${personagem.Titulo}</h2>
+           <p class="card-texto">
+               ${personagem.Descricao}
+            </p>
+        </div>
+    </div> `
+
+}
